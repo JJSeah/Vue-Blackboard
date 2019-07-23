9d5ffa5c08e04944b6e02365f6075fca79a337c1
JJSeah <jjseah00@gmail.com>
2019-07-23 22:22:50 +0800

    responsive design

--- src/components/BlackBoard.vue
+++ src/components/BlackBoard.vue
@@ -7,34 +7,33 @@
 </template>
 
 <script>
-import paper from "paper";
-import MainPanel from "./panel/MainPanel";
-import HistoryPanel from "./panel/HistoryPanel";
-
-export default {
-  components: {
-    mainPanel: MainPanel,
-    historyPanel: HistoryPanel
-  },
-  created() {
-    paper.install(window);
-  },
-  mounted() {
-    paper.setup("blackboard");
-  }
-};
+  import paper from "paper";
+  import MainPanel from "./panel/MainPanel";
+  import HistoryPanel from "./panel/HistoryPanel";
+  export default {
+    components: {
+      mainPanel: MainPanel,
+      historyPanel: HistoryPanel
+    },
+    created() {
+      paper.install(window);
+    },
+    mounted() {
+      paper.setup("blackboard");
+    }
+  };
 </script>
 
 
 <style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-}
-canvas {
-  height: 100%;
-  width: 100%;
-  background: black;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+  canvas {
+    height: 100vh;
+    width: 100%;
+    background: black;
+  }
 </style>
